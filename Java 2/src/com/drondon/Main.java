60a05b7e71979aefb4b64d7174fea30e8e1785a6
Andrii Miroshnychenko <collosteam@gmail.com>
2018-04-02 19:41:31 +0300

    Type cast

--- Java 2/src/com/drondon/Main.java
+++ Java 2/src/com/drondon/Main.java
@@ -3,22 +3,29 @@ package com.drondon;
 public class Main {
 
     public static void main(String[] args) {
-        boolean sameStrings = "I'm string".equals("I'm string ");
-        System.out.println("Same : " + sameStrings);
 
-        boolean sameStrings2 = "I'm string".equals("I'm string");
-        System.out.println("Same 2 : " + sameStrings2);
+        int a = 0;
 
-        String data = "I'm String  ";
-        boolean sameStrings3 = "I'M STRING".equals(data.trim().toUpperCase());
-        System.out.println("Same 3 : " + sameStrings3);
+        long b = 30000000000L;
 
-        String stringData = new String("I'm string");
-        //stringData.trim();
-        //"I'm string".trim();
+        a = (int) b;
 
-        boolean sameStrings4 = "I'm string" == stringData;
-        System.out.println("Same 4 : " + sameStrings4);
+        System.out.println("Result: " + a);
+
+
+        float c;// = 2 / 3.0F;// = 0.0F;
+
+        double d = 2.000010000000000000000000000122231;
+
+        c = (float) d;
+
+        System.out.println("Result 2: " + c);
+
+        String number = "100500";
+        int value  = Integer.valueOf(number);
+
+        System.out.println("Result 3: " + value);
+
 
     }
 
