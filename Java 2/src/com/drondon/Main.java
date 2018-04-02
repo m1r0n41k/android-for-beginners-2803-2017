327c8331f3681c756e1de393d2188a5f56ba49f7
Andrii Miroshnychenko <collosteam@gmail.com>
2018-04-02 21:50:07 +0300

    Arrays and args

--- Java 2/src/com/drondon/Main.java
+++ Java 2/src/com/drondon/Main.java
@@ -5,12 +5,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 4, 5, 6, 7, 8};
-        //                               ^
-        //                       index - 6
-        int index = -1;
-        System.out.println("Array size: " + array.);
-        System.out.println("Array value: [" + index + "] = " + array[index]);
+        System.out.println("User name: " + args[0]);
+        System.out.println("User email: " + args[1]);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Введите слово: ");
+            String input = scanner.next();
+            scanner.reset();
+
+            System.out.println("Length: " + input.length());
+
+            System.out.print("Введите индекс буквы: ");
+            int index = scanner.nextInt();
+
+            char letter = input.charAt(index);
+            System.out.println("Выбранная буква: " + letter);
+
+            char[] chars = input.toCharArray();
+
+            for (int i = 0; i < chars.length; i++) {
+                char c = chars[i];
+                Character upperC = Character.toUpperCase(c);
+                System.out.println("" + upperC + ((i < (chars.length - 1)) ? ", " : ""));
+            }
+
+        }
 
     }
 
