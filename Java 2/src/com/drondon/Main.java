a0c3e91dc2405042ae572f8ea08c24eae5318515
Andrii Miroshnychenko <collosteam@gmail.com>
2018-04-02 20:07:09 +0300

    If else flow

--- Java 2/src/com/drondon/Main.java
+++ Java 2/src/com/drondon/Main.java
@@ -1,35 +1,26 @@
 package com.drondon;
 
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-
-        int a = 0;
-
-        long b = 30000000000L;
-
-        a = (int) b;
-
-        System.out.println("Result: " + a);
-
-
-        float c;// = 2 / 3.0F;// = 0.0F;
-
-        double d = 2.000010000000000000000000000122231;
-
-        c = (float) d;
-
-        System.out.println("Result 2: " + c);
-
-        String number = "100500";
-        int value  = Integer.valueOf(number);
-
-        System.out.println("Result 3: " + value);
-
-
+        printHello();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.next();
+            if (input != null && "exit".equalsIgnoreCase(input) || "quit".equalsIgnoreCase(input)) {
+                System.out.println("Goodbye my friend!");
+                break;
+            } else if ("hello".equalsIgnoreCase(input)) {
+                System.out.println("Hello my friend!");
+            } else {
+                System.out.println("What ?!");
+                printHello();
+            }
+        }
     }
 
-    public static void play() {
-
+    public static final void printHello() {
+        System.out.print("Hello... Please input here: ");
     }
 }
