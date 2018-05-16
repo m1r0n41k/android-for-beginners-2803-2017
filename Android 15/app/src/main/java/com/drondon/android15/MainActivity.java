3136df7f5cf37d486080da99eab729b28952e0e0
Andrii Miroshnychenko <collosteam@gmail.com>
2018-05-16 21:11:14 +0300

    Added bottom navigation

--- Android 15/app/src/main/java/com/drondon/android15/MainActivity.java
+++ Android 15/app/src/main/java/com/drondon/android15/MainActivity.java
@@ -1,32 +1,49 @@
 package com.drondon.android15;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    boolean progress = true;
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottomNavigation);
 
-        final RequiredEditText editText = findViewById(R.id.editText);
-        final ProgressButton button = findViewById(R.id.button);
+        ColorStateList colorStateList = ContextCompat.getColorStateList(this, R.color.app_navigation_colors);
+        bottomNavigation.setItemIconTintList(colorStateList);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                if (progress) {
-                    button.hideProgress();
-                } else {
-                    button.showProgress();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.one:
+                        Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.two:
+                        Toast.makeText(MainActivity.this, "Two", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.three:
+                        Toast.makeText(MainActivity.this, "Three", Toast.LENGTH_SHORT).show();
+                        break;
                 }
-                progress = !progress;
-                Toast.makeText(v.getContext(), "Valid = " + editText.validate(), Toast.LENGTH_SHORT).show();
+
+                return false;
             }
         });
+
     }
 }
